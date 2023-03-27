2e6903623092316984c2b408f7e6ce81b4cb68ec
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-27 13:41:29 +0300

    created migrations,seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#seeds
+
+# generating 10 random heroes
+10.times do
+    hero = Hero.create(
+        name: Faker::Superhero.name,
+        super_name: Faker::Superhero.descriptor
+    )
+end
+
+#generating 10 random powers
+10.times do
+    power = Power.create(
+      name: Faker::Superhero.power,
+      description: Faker::Lorem.sentence
+    )
+  end
+
+  #generating 10 random super_powers
+10.times do
+    hero_power = HeroPower.create(
+        power_id: rand(1..10),
+        hero_id: rand(1..10),
+        strength: ['strong', 'weak', 'average'].sample
+    )
+end
